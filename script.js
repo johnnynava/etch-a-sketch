a90447b783f312ddaae85fe4f0fc400e6b6a16d3
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-19 22:50:42 +0100

    Finish step 5

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 const grid = document.querySelector("div.grid");
+const input = document.querySelector("input");
+let gridSquares = document.querySelectorAll("div.gridSquare");
+let isMouseDown = false;
+let gridSize = 16;
 
-for (let i=0; i<256; i++){
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("gridSquare");
-    grid.appendChild(gridSquare);   
-}
+input.addEventListener("click", function(e){
+    grid.innerHTML = "";
+    gridSize = e.target.value;
+    gridConstruction(gridSize);
+});
 
-const gridSquare = document.querySelectorAll("div.gridSquare");
-gridSquare.forEach(grid => grid.addEventListener("mouseenter", function(e){
-    grid.classList.add("gridHover");
-}));
+function gridConstruction(gridSize){
+    for (let i=0; i<(gridSize*gridSize); i++){
+        const Square = document.createElement("div");
+        Square.classList.add("gridSquare");
+        newSize = (660/gridSize)-1;
+        Square.style.height = `${newSize}px`;
+        Square.style.width = `${newSize}px`;
+        grid.appendChild(Square);
+    }
+    gridSquares = document.querySelectorAll("div.gridSquare");
+    gridSquares.forEach(square => {
+        square.addEventListener("mousemove", function(e){
+            if (isMouseDown === true){
+                square.classList.add("gridHover");
+            }
+        });
+        square.addEventListener("mousedown", function(e){
+            isMouseDown = true;
+            square.classList.add("gridHover");
+        });
+        square.addEventListener("mouseup", function(e){
+            isMouseDown = false;
+        })
+    });
+};
+
+gridConstruction(gridSize);
+
+
+
+
+
