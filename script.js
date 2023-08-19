03610ee19f6a9339bc21d34c7a59051b7de8e5a2
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-19 23:21:24 +0100

    Work towards reset button & random color function

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const grid = document.querySelector("div.grid");
 const input = document.querySelector("input");
+const reset = document.querySelector("button#reset");
+const random = document.querySelector("button#random");
 let gridSquares = document.querySelectorAll("div.gridSquare");
+let gridSizeText = document.querySelector("div#gridSizeText");
 let isMouseDown = false;
 let gridSize = 16;
+
+// reset.addEventListener("click", function(e){
+// })
 
 input.addEventListener("click", function(e){
     grid.innerHTML = "";
     gridSize = e.target.value;
     gridConstruction(gridSize);
+    gridSizeText.textContent = `Grid Size: ${gridSize}x${gridSize}`;
 });
 
 function gridConstruction(gridSize){
